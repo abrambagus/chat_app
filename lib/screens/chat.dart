9c3edda46fa6f5fdb313340592062f31ca0b2f60
abram <abrambagus@gmail.com>
2024-02-27 11:28:24 +0700

    add push norification feature from firebase

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,10 +1,30 @@
 import 'package:chat_app/widgets/chat_messages.dart';
 import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatsScreen extends StatelessWidget {
+class ChatsScreen extends StatefulWidget {
   const ChatsScreen({super.key});
+
+  @override
+  State<ChatsScreen> createState() => _ChatsScreenState();
+}
+
+class _ChatsScreenState extends State<ChatsScreen> {
+  void setupPushnotification() async {
+    final fcm = FirebaseMessaging.instance;
+
+    await fcm.requestPermission();
+
+    fcm.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushnotification();
+  }
 
   @override
   Widget build(BuildContext context) {
